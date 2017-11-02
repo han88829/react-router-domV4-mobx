ee7d7f195f5f6c3e306a39df15778701b655dc45
han092298 <h88829@foxmail.com>
2017-11-02 13:23:53 +0800

    新增

---
+++ src/component/report/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Report extends Component {
+    render() {
+        return (
+            <div>
+                报表
+            </div>
+        );
+    }
+}
+
+export default Report;
---
+++ src/component/rtsp/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Rtsp extends Component {
+    render() {
+        return (
+            <div>
+                Rtsp视频播放流
+            </div>
+        );
+    }
+}
+
+export default Rtsp;
