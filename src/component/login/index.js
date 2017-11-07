c59356de03c84260dac273c2f0d102541c9f7e6d
han092298 <h88829@foxmail.com>
2017-11-07 10:19:41 +0800

    edit

--- src/component/login/index.js
+++ src/component/login/index.js
@@ -38,33 +38,35 @@ class NormalLoginForm extends React.Component {
     render() {
         const { getFieldDecorator } = this.props.form;
         return (
-            <Form onSubmit={this.handleSubmit} className="login-form">
-                <FormItem>
-                    {getFieldDecorator('userName', {
-                        rules: [{ required: true, message: '请输入用户名!' }],
-                    })(
-                        <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="用户名" />
-                        )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator('password', {
-                        rules: [{ required: true, message: '请输入密码!' }],
-                    })(
-                        <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="密码" />
-                        )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator('remember', {
-                        valuePropName: 'checked',
-                        initialValue: true,
-                    })(
-                        <Checkbox>记住密码</Checkbox>
-                        )}
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                        登录
+            <div className="login">
+                <Form onSubmit={this.handleSubmit} className="login-form">
+                    <FormItem>
+                        {getFieldDecorator('userName', {
+                            rules: [{ required: true, message: '请输入用户名!' }],
+                        })(
+                            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="用户名" />
+                            )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('password', {
+                            rules: [{ required: true, message: '请输入密码!' }],
+                        })(
+                            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="密码" />
+                            )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('remember', {
+                            valuePropName: 'checked',
+                            initialValue: true,
+                        })(
+                            <Checkbox style={{ color: "#fff" }}>记住密码</Checkbox>
+                            )}
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            登录
                     </Button>
-                </FormItem>
-            </Form>
+                    </FormItem>
+                </Form>
+            </div>
         );
     }
 }
